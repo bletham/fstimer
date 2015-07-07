ab88985ecf7181f6ac5bd39033e652f72ab5ac0b
bletham <bletham@gmail.com>
2015-07-06 20:56:05 -0700

    prep and files for using pynsist to create windows executable

--- fstimer_init.py
+++ fstimer_init.py
@@ -3,11 +3,15 @@
 import fstimer.timer
 from gi.repository import Gtk
 
-if __name__ == '__main__':
+def main():
     pytimer = fstimer.timer.PyTimer()
     #These next two lines are to get icons on stock buttons in windows
     settings = Gtk.Settings.get_default()
     settings.props.gtk_button_images = True
     Gtk.main()
+
+
+if __name__ == '__main__':
+    main()
 
 
---
+++ gi_build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Extracts all dependencies and places them in the pynsist_pkgs folder
+
+mkdir pynsist_pkgs
+
+# Unzip the bindings
+7z x pygi.exe -opygi
+
+# Copy the PyGI packages into the pynsist_pkgs folder
+7z x pygi/binding/py3.4-64/py3.4-64.7z -obindings
+cp -r bindings/* pynsist_pkgs
+rm -r bindings
+
+# Copy the noarch and specified architecture dependencies into the gnome folder
+array=( ATK Base GDK GDKPixbuf GTK JPEG Pango WebP TIFF )
+
+for i in "${array[@]}"
+do
+    echo -e "\nProcessing $i dependency"
+    7z x pygi/noarch/$i/$i.data.7z -o$i-noarch
+    cp -r $i-noarch/gnome/* pynsist_pkgs/gnome
+    rm -r $i-noarch
+
+    7z x pygi/rtvc10-64/$i/$i.bin.7z -o$i-arch
+    cp -r $i-arch/gnome/* pynsist_pkgs/gnome
+    rm -r $i-arch
+done
+
+#Remove pygi Folder
+rm -r pygi
+
+#Compile glib schemas
+glib-compile-schemas pynsist_pkgs/gnome/share/glib-2.0/schemas/
